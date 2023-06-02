607aa3ddc1edad62108659f1fbb241452faf5628
Matteus Varlesse <varlesse04@gmail.com>
2023-06-02 10:41:51 -0300

    feat: first commit

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,22 @@
+import { randomUUID } from "crypto"
+
+interface QuestionProps {
+  title: string
+  content: string
+  authorId: string
+}
+
+export class Question {
+  public id: string
+  public title: string
+  public content: string
+  public authorId: string
+
+  constructor({ authorId, content, title }: QuestionProps, id?: string) {
+    this.title = title
+    this.content = content
+    this.authorId = authorId
+    this.id = id ?? randomUUID()
+
+  }
+}
---
+++ src/domain/use-cases/answer-questions.ts
@@ -0,0 +1,16 @@
+import { Answer } from "../entities/answer"
+
+interface AnswerQuestionsUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionsUseCase {
+  execute({ instructorId, questionId, content }: AnswerQuestionsUseCaseRequest) {
+    const answer = new Answer(content, instructorId, questionId)
+
+    return answer
+  }
+}
+
