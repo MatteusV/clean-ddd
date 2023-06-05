69dae1e4f16bc6bb84fa059f171d1ec45808ec74
Matteus varlesse <varlesse04@gmail.com>
2023-06-05 15:58:56 -0300

    Abstraindo criação de entidades

--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,4 +1,6 @@
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
+import { Optional } from '../../core/types/optional'
 
 interface StudentProps {
   name: string
@@ -6,5 +8,9 @@ interface StudentProps {
 
 export class Student extends Entity<StudentProps> {
 
+  static create(props: StudentProps, id?: UniqueEntityID) {
+    const student = new Student(props, id)
 
+    return student
+  }
 }
